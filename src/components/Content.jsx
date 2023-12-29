8921dfd09e8e8998a963dc36f006948ea928a986
Sonu Subedi <supertrampsonu4@gmail.com>
2023-12-29 20:08:59 +0545

    Change in styling of content-container

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,19 +8,19 @@ import {
 const Content = () => {
   return (
     <div className="content-container">
-      <div className="iamge1">
+      <div className="image1">
         <img src={bannerimg1} alt="bannerimage" />
       </div>
       <div className="banner-right-images">
         <div className="banner-rowimages">
-          <div className="iamge1">
+          <div className="image2">
             <img src={bannerimg2} alt="bannerimage" />
           </div>
-          <div className="iamge1">
+          <div className="image3">
             <img src={bannerimg3} alt="bannerimage" />
           </div>
         </div>
-        <div className="iamge1">
+        <div className="image4">
           <img src={bannerimg4} alt="bannerimage" />
         </div>
       </div>
