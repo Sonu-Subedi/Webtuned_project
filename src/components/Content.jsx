891f1cb5c58443180d7c0dcbc77d18958052a404
Sonu Subedi <supertrampsonu4@gmail.com>
2023-12-29 20:28:31 +0545

    Add bannertext

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,24 +7,33 @@ import {
 
 const Content = () => {
   return (
-    <div className="content-container">
-      <div className="image1">
-        <img src={bannerimg1} alt="bannerimage" />
-      </div>
-      <div className="banner-right-images">
-        <div className="banner-rowimages">
-          <div className="image2">
-            <img src={bannerimg2} alt="bannerimage" />
+    <>
+      <div className="content-container">
+        <div className="banner-left-images">
+          <div className="image1">
+            <img src={bannerimg1} alt="bannerimage" />
           </div>
-          <div className="image3">
-            <img src={bannerimg3} alt="bannerimage" />
+          <div className="bannertext">
+            <span className="bannertext1">New Style Design</span>
+            <span className="bannertext2">Trendy Fashion</span>
+            <span className="bannertext3">30% off</span>
           </div>
         </div>
-        <div className="image4">
-          <img src={bannerimg4} alt="bannerimage" />
+        <div className="banner-right-images">
+          <div className="banner-rowimages">
+            <div className="image2">
+              <img src={bannerimg2} alt="bannerimage" />
+            </div>
+            <div className="image3">
+              <img src={bannerimg3} alt="bannerimage" />
+            </div>
+          </div>
+          <div className="image4">
+            <img src={bannerimg4} alt="bannerimage" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
